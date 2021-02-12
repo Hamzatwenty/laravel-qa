fb86a4cabfad939b0d12c6bc29a1c7fb5ded71da
Muhammad Hamza <Hamzatwenty>
2021-02-13 00:05:45 +0500

    question ask and show is updated

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -2,21 +2,21 @@
 
 @section('content')
     <style>
-    .counters{
-        margin-right: 30px;
-        font-size: 10px;
-        text-align: center;
-    }
+        .counters{
+            margin-right: 30px;
+            font-size: 10px;
+            text-align: center;
+        }
 
-    .counters strong {
-        display: block;
-        font-size: 2em;
-    }
+        .counters strong {
+            display: block;
+            font-size: 2em;
+        }
 
-    .vote, .answer {
-        width: 60px;
-        height: 60px;
-    }
+        .vote, .answer {
+            width: 60px;
+            height: 60px;
+        }
 
     </style>
     <div class="container">
@@ -25,7 +25,7 @@
                 <div class="card">
                     <div class="card-header">
                         <div class="d-flex align-items-center">
-                            <h2>Ask Question</h2>
+                            <h1>{{ $question->title }}</h1>
                             <div class="ml-auto">
                                 <a href="{{ route('questions.index') }}" class="btn btn-outline-primary">Back to all questions</a>
                             </div>
@@ -33,9 +33,7 @@
                     </div>
 
                     <div class="card-body">
-                        <form action="{{ route('questions.store') }}" method="POST">
-                            @include("questions._form",['buttonText' => "Ask Question"])
-                        </form>
+                        {!! $question->body_html !!}
                     </div>
                 </div>
             </div>
