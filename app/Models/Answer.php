8c4f31b725ffe654cfc32820d5815da1a2cb93cb
Muhammad Hamza <Hamzatwenty>
2021-03-23 13:08:52 +0500

    update best answer id column when an answer is removed through migration

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -17,12 +17,12 @@ class Answer extends Model
             $answer->question->increment('answers_count');
         });
         static::deleted(function ($answer){
-            $question = $answer->question;
-            $question->decrement('answers_count');
-            if ($question->best_answer_id === $answer->id){
+            /*$question = $answer->question;*/
+            $answer->question->decrement('answers_count');
+            /*if ($question->best_answer_id === $answer->id){
                 $question->best_answer_id = null;
                 $question->save();
-            }
+            }*/
         });
     }
 
